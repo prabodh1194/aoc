94e4c2460cee0b3ad9dafe950e53a00dbb0871cd
Prabodh Agarwal <prabodh1194@gmail.com>
2021-01-16 15:20:01 +0530

    add 2 more stars

--- 2020/d3/d31.go
+++ 2020/d3/d31.go
@@ -6,7 +6,33 @@ import (
 )
 
 func D31() {
+	var frst [][]byte
 	for txt := range utils.FileReader("d3/d3.inp") {
-		fmt.Println(txt)
+		var row []byte
+		for i := 0; i < len(txt); i++ {
+			row = append(row, txt[i])
+		}
+		frst = append(frst, row)
 	}
+
+	cnt := 1
+
+	cnt *= getSlope(frst, 1, 1)
+	cnt *= getSlope(frst, 1, 3)
+	cnt *= getSlope(frst, 1, 5)
+	cnt *= getSlope(frst, 1, 7)
+	cnt *= getSlope(frst, 2, 1)
+
+	fmt.Println(cnt)
+}
+
+func getSlope(frst [][]byte, ds int, rs int) int {
+	cnt := 0
+
+	for i, j := 0, 0; i < len(frst); i, j = i+ds, (j+rs)%len(frst[i]) {
+		if frst[i][j] == 35 {
+			cnt++
+		}
+	}
+	return cnt
 }
